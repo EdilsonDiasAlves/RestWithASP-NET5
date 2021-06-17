bba07f2ec938c5fc0df36666f68dc5a3192a16df
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-06-17 18:03:02 -0300

    Completing the implementation of the other verbs:
    1. FindById, Update and Delete - all with db integration

--- RestWithASP-NET5/Services/Impl/PersonServiceImpl.cs
+++ RestWithASP-NET5/Services/Impl/PersonServiceImpl.cs
@@ -16,16 +16,6 @@ namespace RestWithASP_NET5.Services.Impl
         {
             _context = context;
         }
-
-        public Person Create(Person person)
-        {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-        }
-
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
@@ -33,19 +23,63 @@ namespace RestWithASP_NET5.Services.Impl
 
         public Person FindById(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault<Person>(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = id,
-                FirstName = "John",
-                LastName = "Doe",
-                Address = "São Paulo - SP - Brasil",
-                Gender = "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault<Person>(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+            var result = _context.Persons.SingleOrDefault<Person>(p => p.Id.Equals(person.Id));
+
+            if(result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
             return person;
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
